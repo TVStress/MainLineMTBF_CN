4bb2c495d1b06e79b1be99df690ecc2079ab9614
ext-yuren.zhang <ext-yuren.zhang@le.com>
2018-04-09 18:09:20 +0800

    StressScript

--- app/src/androidTest/java/com/letv/cases/leui/DeskManagerStress.java
+++ app/src/androidTest/java/com/letv/cases/leui/DeskManagerStress.java
@@ -97,7 +97,6 @@ public class DeskManagerStress extends LetvTestCase{
         }
         press_back(2);
     }
-
     public void DeskSwitch() throws UiObjectNotFoundException, RemoteException {
         addStep("调整开机默认桌面");
         UiObject2 home=waitForObj(By.res("com.stv.launcher:id/recycler_view"));
@@ -129,7 +128,6 @@ public class DeskManagerStress extends LetvTestCase{
             press_back(1);
         }
     }
-
     public void DeskSwitch938() throws UiObjectNotFoundException, RemoteException {
 
         UiObject2 bigc =waitForObj(By.res("com.stv.launcher:id/rlv_to_add")).findObject(By.res("com.stv.launcher:id/tv_title").text("小C精选"));
@@ -174,7 +172,4 @@ public class DeskManagerStress extends LetvTestCase{
         }
         press_left(9);
     }
-
-
-
 }
